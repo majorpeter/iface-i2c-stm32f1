844ba7baa9205569f4b13d0713e2ece692c307b2
Major Péter <majorpeter29@gmail.com>
2018-05-22 20:41:16 +0200

    removed hard-coded definitions

--- Stm32F1I2c.cpp
+++ Stm32F1I2c.cpp
@@ -11,50 +11,71 @@
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
 
-#define I2Cx_RCC        RCC_APB1Periph_I2C1
-#define I2Cx            I2C1
-#define I2C_GPIO_RCC    RCC_APB2Periph_GPIOB
-#define I2C_GPIO        GPIOB
-#define I2C_PIN_SDA     GPIO_Pin_7
-#define I2C_PIN_SCL     GPIO_Pin_6
+#include <assert.h>
 
-Stm32F1I2c::Stm32F1I2c() {
-    this->init();
-}
+Stm32F1I2c::Stm32F1I2c(const InitStruct* initStruct) {
+    i2c = initStruct->i2cPeriph;
 
-void Stm32F1I2c::init() {
-    RCC_APB1PeriphClockCmd(I2Cx_RCC, ENABLE);
+    assert(rccEnable(i2c, initStruct->sdaSclPort) == 0);
+
     I2C_InitTypeDef I2C_InitStruct;
-    I2C_InitStruct.I2C_ClockSpeed = 100000;
+    I2C_InitStruct.I2C_ClockSpeed = initStruct->clockSpeedHz;
     I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
     I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
     I2C_InitStruct.I2C_OwnAddress1 = 0x00;
     I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
     I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    I2C_Init(I2Cx, &I2C_InitStruct);
-    I2C_Cmd(I2Cx, ENABLE);
+    I2C_Init(i2c, &I2C_InitStruct);
+    I2C_Cmd(i2c, ENABLE);
 
-    RCC_APB2PeriphClockCmd(I2C_GPIO_RCC, ENABLE);
     GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.GPIO_Pin = I2C_PIN_SCL | I2C_PIN_SDA;
+    GPIO_InitStruct.GPIO_Pin = initStruct->sdaPin | initStruct->sclPin;
     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_OD;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(I2C_GPIO, &GPIO_InitStruct);
+    GPIO_Init(initStruct->sdaSclPort, &GPIO_InitStruct);
+}
+
+int8_t Stm32F1I2c::rccEnable(const I2C_TypeDef* i2c, const GPIO_TypeDef* sdaSclPort) {
+    if (i2c == I2C1) {
+        RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+    } else if (i2c == I2C2) {
+        RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+    } else {
+        return -1;
+    }
+
+    if (sdaSclPort == GPIOA) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    } else if (sdaSclPort == GPIOB) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    } else if (sdaSclPort == GPIOC) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    } else if (sdaSclPort == GPIOD) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+    } else if (sdaSclPort == GPIOE) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE);
+    } else if (sdaSclPort == GPIOF) {
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOF, ENABLE);
+    } else {
+        return -2;
+    }
+
+    return 0;
 }
 
 void Stm32F1I2c::sendStart() {
-    while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY));
-    I2C_GenerateSTART(I2Cx, ENABLE);
-    while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_MODE_SELECT));
+    while (I2C_GetFlagStatus(i2c, I2C_FLAG_BUSY));
+    I2C_GenerateSTART(i2c, ENABLE);
+    while (!I2C_CheckEvent(i2c, I2C_EVENT_MASTER_MODE_SELECT));
 }
 
 void Stm32F1I2c::sendStop() {
-    I2C_GenerateSTOP(I2Cx, ENABLE);
-    while (I2C_GetFlagStatus(I2Cx, I2C_FLAG_STOPF));
+    I2C_GenerateSTOP(i2c, ENABLE);
+    while (I2C_GetFlagStatus(i2c, I2C_FLAG_STOPF));
 }
 
 int8_t Stm32F1I2c::sendSlaveAddress(uint8_t address, uint8_t direction) {
-    I2C_Send7bitAddress(I2Cx, address, direction);
+    I2C_Send7bitAddress(i2c, address, direction);
 
     uint32_t i2c_event = 0;
     if (direction == I2C_Direction_Transmitter) {
@@ -64,7 +85,7 @@ int8_t Stm32F1I2c::sendSlaveAddress(uint8_t address, uint8_t direction) {
     }
 
     uint32_t timeout = 100000;
-    while (!I2C_CheckEvent(I2Cx, i2c_event)) {
+    while (!I2C_CheckEvent(i2c, i2c_event)) {
         timeout--;
         if (timeout == 0) {
             return Nack;
@@ -74,15 +95,15 @@ int8_t Stm32F1I2c::sendSlaveAddress(uint8_t address, uint8_t direction) {
 }
 
 void Stm32F1I2c::sendByte(uint8_t byte) {
-    I2C_SendData(I2Cx, byte);
-    while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+    I2C_SendData(i2c, byte);
+    while (!I2C_CheckEvent(i2c, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
 }
 
 uint8_t Stm32F1I2c::receiveByte(bool ack) {
-    I2C_AcknowledgeConfig(I2Cx, ack ? ENABLE : DISABLE);
-    while (!I2C_CheckEvent(I2Cx, I2C_EVENT_MASTER_BYTE_RECEIVED));
+    I2C_AcknowledgeConfig(i2c, ack ? ENABLE : DISABLE);
+    while (!I2C_CheckEvent(i2c, I2C_EVENT_MASTER_BYTE_RECEIVED));
 
-    return I2C_ReceiveData(I2Cx);
+    return I2C_ReceiveData(i2c);
 }
 
 int8_t Stm32F1I2c::write(uint8_t slave_address, const uint8_t* data, uint16_t length) {
--- Stm32F1I2c.h
+++ Stm32F1I2c.h
@@ -9,17 +9,28 @@
 #define MODULES_IFACE_I2C_STM32F1_STM32F1I2C_H_
 
 #include <iface-i2c/I2cInterface.h>
+#include <stm32f10x.h>
 
 class Stm32F1I2c: public I2cInterface {
 public:
     enum Error {Ok = 0, Nack = -1};
-    Stm32F1I2c();
+    typedef struct {
+        I2C_TypeDef* i2cPeriph;
+        GPIO_TypeDef* sdaSclPort;
+        uint16_t sdaPin;
+        uint16_t sclPin;
+        uint32_t clockSpeedHz;
+    } InitStruct;
+
+    Stm32F1I2c(const InitStruct* initStruct);
     virtual ~Stm32F1I2c() {}
 
     virtual int8_t write(uint8_t slave_address, const uint8_t* data, uint16_t length);
     virtual int8_t read(uint8_t slave_address, uint8_t* data, uint16_t length);
 private:
-    void init();
+    I2C_TypeDef* i2c;
+
+    int8_t rccEnable(const I2C_TypeDef* i2c, const GPIO_TypeDef* sdaSclPort);
     void sendStart();
     void sendStop();
     int8_t sendSlaveAddress(uint8_t address, uint8_t direction);
